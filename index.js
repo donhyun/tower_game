45dd992a0b9d4361fcfe14f873bc66ae77d82e0d
Don Hyun <donhyun@hotmail.com>
2022-09-28 22:53:43 -0700

    high scores add pics

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require('path')
 const opn = require('opn')
 
 const server = express()
+
 const host = 'http://localhost:8082'
 server.use('/assets', express.static(path.resolve(__dirname, './assets')))
 server.use('/dist', express.static(path.resolve(__dirname, './dist')))
 
-server.get('*', (req, res) => {
+server.get('*',(req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'));
 })
 
